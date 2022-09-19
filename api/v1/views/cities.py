08ff861a433dece855150470adfc492e03d55296
alejuran <alejandrourn@gmail.com>
2022-09-19 08:46:44 -0700

    update cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Update states"""
+"""objects that handles all default RestFul API actions for cities"""
 
 from flask import abort, request, jsonify, make_response
 from api.v1.views import app_views
